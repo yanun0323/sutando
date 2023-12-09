b38c5b27c1871ceafd9984b4a377a5b1b37b7e7d
yanun0323 <a1256212562@gmail.com>
2023-12-09 19:52:26 +0800

    rm unused func

--- bson.go
+++ bson.go
@@ -6,10 +6,6 @@ func bsonD(elems ...bson.E) bson.D {
 	return append(bson.D{}, elems...)
 }
 
-func bsonE(key string, val any) bson.E {
-	return bson.E{Key: key, Value: val}
-}
-
 func bsonM(key string, val any) bson.M {
 	return bson.M{key: val}
 }
