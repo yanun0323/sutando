c8b21929241f54e886d4ed59786a5ef47ffe822a
yanun0323 <a1256212562@gmail.com>
2023-12-10 02:09:59 +0800

    add comment for option regex

--- option/regex.go
+++ option/regex.go
@@ -3,8 +3,37 @@ package option
 type Regex int
 
 const (
-	CaseInsensitive  Regex = 1
-	MatchMultiLine   Regex = 1 << 1
+	// CaseInsensitive makes case insensitivity to match upper and lower cases.
+	CaseInsensitive Regex = 1
+
+	// MatchMultiLine includes anchors
+	// (i.e. ^ for the start, $ for the end),
+	// match at the beginning or end of each line for
+	// strings with multiline values. Without this option,
+	// these anchors match at beginning or end of the string.
+	//
+	// If the pattern contains no anchors or
+	// if the string value has no newline characters
+	// (e.g. \n), the m option has no effect.
+	MatchMultiLine Regex = 1 << 1
+
+	// IgnoreWhitespace ignores all white space characters
+	// in the $regex pattern unless escaped
+	// or included in a character class.
+	//
+	// Additionally, it ignores characters in-between
+	// and including an un-escaped hash/pound (#) character
+	// and the next new line, so that you may include comments
+	// in complicated patterns. This only applies to
+	// data characters; white space characters may never appear
+	// within special character sequences in a pattern.
+	//
+	// The x option does not affect the handling
+	// of the VT character (i.e. code 11).
 	IgnoreWhitespace Regex = 1 << 2
-	DotMatchNewLine  Regex = 1 << 3
+
+	// Allows the dot character (i.e. .)
+	// to match all characters
+	// including newline characters.
+	DotMatchNewLine Regex = 1 << 3
 )
