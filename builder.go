972c047fcfcd4955a0d159683d58dd29627fea7f
yanun0323 <a1256212562@gmail.com>
2023-12-09 20:04:45 +0800

    update builder comment

--- builder.go
+++ builder.go
@@ -9,56 +9,56 @@ type builder struct {
 }
 
 /*
-Set Custom Encode Types/Structure
+SetCustomEncodeTypes sets custom encode types/structure
 */
 func (b builder) SetCustomEncodeTypes() *builder {
 	return &b
 }
 
 /*
-Insert data
+Insert inserts data
 */
 func (b builder) Insert(p ...any) inserting {
 	return newInsert(b.col, newBsonEncoder(), p...)
 }
 
 /*
-Update data with model (Will update all fields including empty fields)
+Update updates data with model (Will update all fields including empty fields)
 */
 func (b builder) UpdateWith(p any) updating {
 	return newUpdate(b.col, newBsonEncoder(), p)
 }
 
 /*
-Update data with set
+Update updates data with set
 */
 func (b builder) Update() updating {
 	return newUpdate(b.col, newBsonEncoder(), nil)
 }
 
 /*
-Find data
+Find finds data
 */
 func (b builder) Find() finding {
 	return newFind(b.col)
 }
 
 /*
-Delete data
+Delete deletes data
 */
 func (b builder) Delete() deleting {
 	return newDelete(b.col)
 }
 
 /*
-Scalar data
+Scalar scalars data
 */
 func (b builder) Scalar() scalaring {
 	return newScalar(b.col)
 }
 
 /*
-For Query Test
+query is for package testing
 */
 func (b builder) query() querying {
 	return newQuery(b.col)
